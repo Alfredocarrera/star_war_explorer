caef2ce0d406e5fdb086d40e32a67b922c85fe13
Alfredo Carrera <fredydelta19@gmail.com>
2026-07-27 00:46:49 -0600

    feat: creacion de elementos en javascript he intregrados en el archivo index html imagen de cabecera

--- js/inicio.js
+++ js/inicio.js
@@ -1,1 +1,26 @@
 // creacion de elemento de pagina index
+
+// creando contenedor principal de la cabecera
+const cabecera = document.createElement('div');  // creando elemento div
+cabecera.classList.add('contedorCabecera');
+
+// crando elemento contenedor de la imagen (tarjeta)
+const tarjeta = document.createElement('div'); // creando contenedor que estara en la parte superio del la pagina para contener una imagen.
+tarjeta.classList.add('tarjetaLogo');
+
+//crear elemento imagen
+const imagenSup = document.createElement('img');  // creando objeto imagen
+imagenSup.src = './img/logo_sup.jpg';
+imagenSup.alt = 'titulo';
+imagenSup.classList.add('logo');
+
+//cargar la imagen en el contenedor div
+tarjeta.appendChild(imagenSup); //colocando hijo en el contendor en la parte inical
+
+// cargando hijo en contenedor cabecera
+cabecera.appendChild(tarjeta);
+
+// cargando al contenedor body del archivo index.html
+document.body.appendChild(cabecera);
+
+
